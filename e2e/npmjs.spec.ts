344e6f142d1b644707aca0c6ff8b8f012f38f0f2
superwf <superwf@gmail.com>
2022-08-03 15:01:44 +0800

    build: test case

--- e2e/npmjs.spec.ts
+++ e2e/npmjs.spec.ts
@@ -23,22 +23,12 @@ test('test', async ({ page }) => {
   // Click [placeholder="Search packages"]
   await page.locator('[placeholder="Search packages"]').click()
 
-  // Fill [placeholder="Search packages"]
-  await page.locator('[placeholder="Search packages"]').fill('playwright')
-
-  // Click text=playwrightA high-level API to automate web browsers >> strong
-  await page.locator('text=playwrightA high-level API to automate web browsers >> strong').click()
-
-  // Click button:has-text("Search")
-  await page.locator('button:has-text("Search")').click()
-  await expect(page).toHaveURL('https://www.npmjs.com/search?q=playwright')
-
-  // Click text=playwrightexact match >> h3
-  await page.locator('text=playwrightexact match >> h3').click()
-  await expect(page).toHaveURL('https://www.npmjs.com/package/playwright')
+  // Go to https://www.npmjs.com/package/playwright
+  await page.goto('https://www.npmjs.com/package/playwright')
 
   // Click img[alt="TypeScript icon\, indicating that this package has built-in type declarations"]
   await page
     .locator('img[alt="TypeScript icon\\, indicating that this package has built-in type declarations"]')
     .click()
 })
+
