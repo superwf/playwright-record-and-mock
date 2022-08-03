7a1c12845fb40f1c192cba817a5535ea9937fc85
superwf <superwf@gmail.com>
2022-08-03 14:07:03 +0800

    refactor: add more record data attr

--- src/helper.ts
+++ src/helper.ts
@@ -1,3 +1,5 @@
 export const encodeToBase64 = (str: string | Buffer): string => Buffer.from(str).toString('base64')
 
 export const decodeFromBase64 = (base64str: string): Buffer => Buffer.from(base64str, 'base64')
+
+export const isContentTypeText = (contentType: string) => /text|script|xml|xhtml/.test(contentType)
