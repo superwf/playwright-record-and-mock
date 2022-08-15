6204aba4248ff360285b450f286dbeb0caa555b4
superwf <superwf@gmail.com>
2022-08-15 15:23:36 +0800

    test: pkg files must include init config file

--- src/initUserConfig.test.ts
+++ src/initUserConfig.test.ts
@@ -3,9 +3,11 @@ import path from 'path'
 import { initUserConfig } from './initUserConfig'
 import { CONFIG_FILE_NAME } from './constant'
 import { testInTempPath } from './test-tool'
+import pkg from '../package.json'
 
 test('create initUserConfig file', async () => {
   expect(CONFIG_FILE_NAME).toBe('playwright-record-and-mock.config.ts')
+  expect(pkg.files.includes(CONFIG_FILE_NAME)).toBe(true)
   testInTempPath(testPath => {
     const configTargetFile = path.join(testPath, CONFIG_FILE_NAME)
     expect(fs.existsSync(configTargetFile)).toBe(false)
